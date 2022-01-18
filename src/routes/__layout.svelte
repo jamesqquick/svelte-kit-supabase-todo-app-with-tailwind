76d1fd9a25e7a0bafb8a1d9272fcd1a6242b5a69
James Q Quick <james.q.quick@gmail.com>
2022-01-18 07:58:39 -0600

    removing supabase stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,7 @@
 <script>
     import "../app.css";
-    import {supabase} from "../supabase.js";
-    import {user} from "../stores/authStore.js";
-    import Auth from "../components/Auth.svelte";
-    import Navbar from "../components/Navbar.svelte";
-
-    user.set(supabase.auth.user())
-
-    supabase.auth.onAuthStateChange(( _, session) => {
-        user.set(session?.user);
-    });
 </script>
 
 <div class="container mx-auto my-6 max-w-lg">        
-    {#if $user}
-        <Navbar/>
-        <slot></slot>
-    {:else }
-        <Auth/>
-    {/if}
+        <slot></slot>   
 </div>
